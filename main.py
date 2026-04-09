caa93fc8bfa817efc4af09948c9113d9de42da50
İlker KAYA <kayailker.0505@gmail.com>
2026-04-09 16:49:03 +0300

    The folder creation feature has been added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import Image, ImageTk
 
 window = Tk()
@@ -34,12 +35,29 @@ label_key.pack(pady=(20, 0))
 entry_key = Entry(width=40)
 entry_key.pack()
 
+def import_file():
+    entry_get = entry_title.get().strip()
+    text_get = text.get("1.0", END).strip()
+
+    if not text_get or not entry_get:
+        messagebox.showinfo("Error", "Please enter both title and text")
+        return
+
+    file_name = "MySecret.txt"
+    with open(file_name, "w", encoding="utf-8") as f:
+        f.write(entry_get)
+        f.write("\n")
+        f.write(text_get)
+    messagebox.showinfo("Secret Notes", "Secret Notes saved successfully!")
+    window.destroy()
+
 #save & encrypt
-button_save_encrypt = Button(text="Save & Encrypt")
+button_save_encrypt = Button(window, text="Save & Encrypt", command=import_file)
 button_save_encrypt.pack(pady=(10, 0))
 
 #Decrypt
 button_decrypt = Button(text="Decrypt")
 button_decrypt.pack(pady=(10, 0))
+
 
 window.mainloop()
